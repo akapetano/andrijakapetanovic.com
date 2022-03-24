8c491e47fdf68e63616a57c0aa96ca311e1b4634
Andrija Kapetanović <andrija.kapetanovic@gmail.com>
2022-03-24 13:02:55 +0100

    implement getStaticPaths

---
+++ pages/posts/[id].js
@@ -0,0 +1,22 @@
+import Layout from '../../components/layout';
+import { getAllPostIds } from '../../lib/posts';
+
+export default function Post() {
+  return <Layout></Layout>;
+}
+
+export async function getStaticPaths() {
+  // Return a list of possible value for id
+}
+
+export async function getStaticProps({ params }) {
+  // Fetch necessary data for the blog post using params.id
+}
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}
