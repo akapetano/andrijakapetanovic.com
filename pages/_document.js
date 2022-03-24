dbce553a7c5c37c286c51a31f08647a840eb609d
Andrija Kapetanović <andrija.kapetanovic@gmail.com>
2022-03-24 15:34:16 +0100

    change font to montserrat

---
+++ pages/_document.js
@@ -0,0 +1,18 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+export default function MyDocument() {
+  return (
+    <Html>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
