fb3764856b65d0f3251e6d28d773a43f94608558
Andrija Kapetanović <andrija.kapetanovic@gmail.com>
2022-10-25 22:26:48 +0200

    install turbo and update to next 13

--- components/core/NavLink/NavLink.tsx
+++ components/core/NavLink/NavLink.tsx
@@ -2,10 +2,10 @@ import {
   Link as ChakraLink,
   LinkProps,
   useColorModeValue,
-} from '@chakra-ui/react';
-import NextLink from 'next/link';
-import { useRouter } from 'next/router';
-import React from 'react';
+} from "@chakra-ui/react";
+import NextLink from "next/link";
+import { useRouter } from "next/router";
+import React from "react";
 
 interface NavLinkProps extends LinkProps {
   children?: string | React.ReactNode;
@@ -25,7 +25,7 @@ export default function NavLink({
 }: NavLinkProps) {
   const router = useRouter();
   const isActive = router.pathname === to;
-  const linkColor = useColorModeValue('brand.700', 'brand.200');
+  const linkColor = useColorModeValue("brand.700", "brand.200");
   if (isActive) {
     return (
       <NextLink href={to} passHref>
@@ -37,18 +37,18 @@ export default function NavLink({
           {...activeProps}
           _before={{
             content: "''",
-            position: 'absolute',
-            width: '100%',
-            height: '2px',
-            borderRadius: '2px',
-            bottom: '0',
-            left: '0',
-            transform: 'scaleX(100%)',
-            transition: 'transform .3s ease-in-out',
+            position: "absolute",
+            width: "100%",
+            height: "2px",
+            borderRadius: "2px",
+            bottom: "0",
+            left: "0",
+            transform: "scaleX(100%)",
+            transition: "transform .3s ease-in-out",
           }}
           _hover={{
             color: linkColor,
-            textDecoration: 'none',
+            textDecoration: "none",
           }}
         >
           {children}
@@ -63,7 +63,7 @@ export default function NavLink({
         textTransform={textTransform}
         fontWeight={300}
         {...props}
-        _hover={{ textDecoration: 'none', opacity: '1' }}
+        _hover={{ textDecoration: "none", opacity: "1" }}
       >
         {children}
       </ChakraLink>
