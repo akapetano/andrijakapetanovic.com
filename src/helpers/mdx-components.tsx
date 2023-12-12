e240f7d00dcd594834cd136cc85280d4ea225bb0
Andrija Kapetanović <andrija.kapetanovic@gmail.com>
2023-12-12 13:50:57 +0100

    add chakra headings to the mdx-components

--- src/helpers/mdx-components.tsx
+++ src/helpers/mdx-components.tsx
@@ -1,6 +1,10 @@
 import { CodeSnippet } from "../components/core/CodeSnippet/CodeSnippet";
+import { Heading } from "@chakra-ui/react";
 import type { MDXComponents } from "mdx/types";
 
 export const COMPONENT_MAP = {
   pre: CodeSnippet,
+  h1: (props) => <Heading as="h1" size="xl" {...props} />,
+  h2: (props) => <Heading as="h2" size="lg" {...props} />,
+  h3: (props) => <Heading as="h3" size="md" mb={"0.5rem"} {...props} />,
 } as MDXComponents;
