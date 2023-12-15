9f613a662ac3ae69ccf5ce37b26fc4a1deecfd81
Andrija Kapetanović <andrija.kapetanovic@gmail.com>
2023-12-15 14:59:49 +0100

    continue writing second blog post

--- src/helpers/mdx-components.tsx
+++ src/helpers/mdx-components.tsx
@@ -9,6 +9,9 @@ export const COMPONENT_MAP = {
   h1: (props) => <Heading as="h1" size="xl" {...props} />,
   h2: (props) => <Heading as="h2" size="lg" {...props} />,
   h3: (props) => <Heading as="h3" size="md" mb={"0.5rem"} {...props} />,
+  h4: (props) => <Heading as="h4" size="sm" mb={"0.5rem"} {...props} />,
+  h5: (props) => <Heading as="h5" size="xs" mb={"0.5rem"} {...props} />,
+  h6: (props) => <Heading as="h6" size="xs" mb={"0.5rem"} {...props} />,
   a: (props) => (
     <ChakraLink variant={"slidingUnderline"} as={Link} {...props} />
   ),
