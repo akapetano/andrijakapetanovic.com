72273356d96a9b8b5d3027d302891e9dac39b805
Andrija Kapetanović <andrija.kapetanovic@gmail.com>
2024-01-22 13:01:16 +0100

    adjust BlogSection layout

--- src/components/features/blog/BlogSection/BlogSection.tsx
+++ src/components/features/blog/BlogSection/BlogSection.tsx
@@ -9,12 +9,11 @@ import {
   useColorModeValue,
   Link as ChakraLink,
 } from "@chakra-ui/react";
+import { ArrowForwardIcon } from "@chakra-ui/icons";
 import Date from "../BlogPost/Date/Date";
-import NavLink from "../../../core/NavLink/NavLink";
 import { IBlogPost } from "@/types/blog";
 import { Card } from "../../../core/Card/Card";
 import Link from "next/link";
-import { motion } from "framer-motion";
 
 export interface IAllPostsData {
   allPostsData: IBlogPost[];
@@ -24,6 +23,7 @@ export function BlogSection({ allPostsData }: IAllPostsData) {
   const dateColor = useColorModeValue("gray.500", "gray.400");
   const textColor = useColorModeValue("gray.600", "gray.300");
   const titleHoverColor = useColorModeValue("brand.600", "brand.300");
+  const linkHoverColor = useColorModeValue("brand.700", "brand.200");
 
   return (
     <SectionContainer>
@@ -35,48 +35,89 @@ export function BlogSection({ allPostsData }: IAllPostsData) {
         p={0}
         m={0}
         display="grid"
-        gridTemplateColumns={{ base: "1fr", md: "repeat(2, 1fr)" }}
+        gridTemplateColumns={{
+          base: "1fr",
+          md: "repeat(auto-fill, minmax(16rem, 1fr))",
+        }}
         gridAutoRows="1fr"
         gap={{ base: 0, md: 5 }}
       >
-        {allPostsData?.map(({ slug, publishedOn, abstract, title }) => (
-          <ChakraLink
-            key={slug}
-            as={Link}
-            href={`blog/posts/${slug}`}
-            variant="noUnderline"
-            height={"auto"}
-          >
-            <ListItem className="blog-post-list-item" margin="0 0 1.25rem">
-              <Card>
-                <Heading
-                  as={"h3"}
-                  fontWeight="semibold"
+        {allPostsData?.map(({ slug, publishedOn, abstract, title }) => {
+          const articlePath = `blog/posts/${slug}`;
+
+          return (
+            <ListItem
+              key={slug}
+              className="blog-post-list-item"
+              margin="0 0 1.25rem"
+              display={"flex"}
+            >
+              <Card
+                display={"flex"}
+                flexDirection="column"
+                justifyContent={"space-between"}
+              >
+                <ChakraLink
                   fontSize="lg"
-                  pb={"1rem"}
-                  color={textColor}
-                  sx={{
-                    ".blog-post-list-item: hover &": {
-                      color: titleHoverColor,
-                    },
-                  }}
+                  display="flex"
+                  alignItems="center"
+                  gap="0.2rem"
+                  mt="0.5rem"
+                  as={Link}
+                  href={articlePath}
+                  variant="noUnderline"
                 >
-                  {title}
-                </Heading>
+                  <Heading
+                    as={"h3"}
+                    fontWeight="semibold"
+                    fontSize="lg"
+                    mb={"1rem"}
+                    color={textColor}
+                    sx={{
+                      ".blog-post-list-item: hover &": {
+                        color: titleHoverColor,
+                      },
+                    }}
+                  >
+                    {title}
+                  </Heading>
+                </ChakraLink>
                 {abstract && (
                   <Text fontSize="md" color={textColor}>
                     {abstract}
                   </Text>
                 )}
                 {publishedOn && (
-                  <Text fontSize="sm" color={dateColor}>
+                  <Text fontSize="sm" color={dateColor} flexGrow={1}>
                     <Date dateString={publishedOn} />
                   </Text>
                 )}
+                <ChakraLink
+                  fontSize="lg"
+                  display="flex"
+                  alignItems="center"
+                  gap="0.2rem"
+                  py="0.5rem"
+                  className="read-article-button"
+                  as={Link}
+                  href={articlePath}
+                  variant="noUnderline"
+                  _hover={{ color: linkHoverColor }}
+                >
+                  Read article
+                  <ArrowForwardIcon
+                    transition="transform .3s ease-in-out"
+                    sx={{
+                      ".read-article-button: hover &": {
+                        transform: "translateX(30%) scale(1.1)",
+                      },
+                    }}
+                  />
+                </ChakraLink>
               </Card>
             </ListItem>
-          </ChakraLink>
-        ))}
+          );
+        })}
       </UnorderedList>
     </SectionContainer>
   );
