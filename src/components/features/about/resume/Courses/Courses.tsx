0710089a0a8315959b9ec9164eabb7ccb50f0201
Andrija Kapetanović <andrija.kapetanovic@gmail.com>
2024-02-02 17:18:14 +0100

    add new course to resume

--- src/components/features/about/resume/Courses/Courses.tsx
+++ src/components/features/about/resume/Courses/Courses.tsx
@@ -24,6 +24,10 @@ export function Courses() {
       <Divider m="0.5rem 0" />
       <UnorderedList fontSize="medium">
         <ListItem>
+          &quot;Fullstack Svelte with SvelteKit&quot; by Rich Harris - February
+          2024
+        </ListItem>
+        <ListItem>
           &quot;Svelte 3+ Fundamentals, v2&quot; by Rich Harris - January 2024
         </ListItem>
         <ListItem>
